c20585b9fef38a45c84eee3c456a36f55c02e043
Dan Deng <deng.wei@bluesg.com.sg>
2026-01-03 15:52:13 +0700

    (feat) Implement new session button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { GitIcon, VercelIcon } from "./icons";
+import { GitIcon, MessageIcon } from "./icons";
 import Link from "next/link";
+import { useParams, useRouter } from "next/navigation"
+import { useCallback } from "react";
 
 export const Navbar = () => {
+
+  const router = useRouter()
+  const params = useParams()
+
+  const refreshSession = useCallback(async () => {
+    const uuid = crypto.randomUUID()
+    // Redirect to the home page with the UUID appended
+    router.push(`/${uuid}`)
+  }, [router, params]);
+
   return (
     <div className="p-2 flex flex-row gap-2 justify-between">
       <Link href="https://github.com/danthaman44/ai-sdk/">
@@ -13,12 +25,13 @@ export const Navbar = () => {
         </Button>
       </Link>
 
-      <Link href="https://vercel.com/">
-        <Button>
-          <VercelIcon />
-          Powered by Vercel
-        </Button>
-      </Link>
+      <Button onClick={(event) => {
+        event.preventDefault();
+        refreshSession();
+      }}>
+        <MessageIcon />
+        New Session
+      </Button>
     </div>
   );
 };
